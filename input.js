1d943f90424e9e36995b3319f3cb8f2e9193193e
rahul-sijwali <rahulsijwali@gmail.com>
2022-10-01 19:21:16 +0530

    v1

---
+++ input.js
@@ -0,0 +1,34 @@
+export class InputHandler {
+  constructor() {
+    this.keys = [];
+    window.addEventListener("keydown", (e) => {
+      if (
+        (e.key === "ArrowDown" ||
+          e.key === "ArrowUp" ||
+          e.key === "ArrowLeft" ||
+          e.key === "ArrowRight" ||
+          e.key === "ArrowDown" ||
+          e.key === "w") &&
+        this.keys.indexOf(e.key) === -1
+      ) {
+        this.keys.push(e.key);
+      }
+      //console.log(e.key, this.keys);
+    });
+
+    window.addEventListener("keyup", (e) => {
+      if (
+        e.key === "ArrowDown" ||
+        e.key === "ArrowUp" ||
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowRight" ||
+        e.key === "ArrowDown" ||
+        e.key === "w"
+      ) {
+        this.keys.splice(this.keys.indexOf(e.key), 1);
+      }
+
+      //console.log(e.key, this.keys);
+    });
+  }
+}
